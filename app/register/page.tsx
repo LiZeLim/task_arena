e463d1b1751d870117437e48fd1eadd5b8fe67ac
Li Ze Lim <lizelim995@gmail.com>
2024-07-19 12:02:32 +1000

    added initial signup design + initial client side handle submit

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,97 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
-const page = () => {
+const Page = () => {
+    const router = useRouter();
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const name = formData.get("name") as string;
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+        const workoutGoal = formData.get("workoutGoal") as string;
+
+        console.log(name, email, password, workoutGoal);
+    }
+
     return (
-    <div>
-        
-    </div>
-);};
+        <div>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left max-w-96">
+                        <h1 className="text-5xl font-bold">Sign Up now!</h1>
+                        <p className="py-6">Something for now</p>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form className="card-body" onSubmit={handleSubmit}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    placeholder="name"
+                                    name="name"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="email"
+                                    name="email"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="password"
+                                    name="password"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">
+                                        Weekly Workout Goal
+                                    </span>
+                                </label>
+                                <input
+                                    type="number"
+                                    placeholder="workoutGoal"
+                                    name="workoutGoal"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">
+                                    Sign Up
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default page;
+export default Page;
